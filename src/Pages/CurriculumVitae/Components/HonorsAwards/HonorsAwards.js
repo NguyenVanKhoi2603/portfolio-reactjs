3d894f9b5de839c1e6b6e6e504e246802b1a32fd
Khoi Nguyen <khoinguyenvk9@gmail.com>
2021-07-28 09:22:16 +0700

    test

--- src/Pages/CurriculumVitae/Components/HonorsAwards/HonorsAwards.js
+++ src/Pages/CurriculumVitae/Components/HonorsAwards/HonorsAwards.js
@@ -1,7 +1,7 @@
 import { Divider, Card, List, Avatar } from 'antd';
 import React, { Component } from 'react';
 import './HonorsAwards.css';
-import { PieChartOutlined } from "@ant-design/icons";
+import { PieChartOutlined, TrophyOutlined } from "@ant-design/icons";
 
 class HonorsAwards extends Component {
   // constructor(props){
@@ -21,14 +21,9 @@ class HonorsAwards extends Component {
   render() {
     const data = [
       {
-        title: 'Ant Design Title 1',
+        title: 'Second semester scholarship',
       },
-      {
-        title: 'Ant Design Title 2',
-      },
-      {
-        title: 'Ant Design Title 3',
-      },
+
     ];
     return (
       <div className="honor-awards">
@@ -41,8 +36,9 @@ class HonorsAwards extends Component {
             renderItem={item => (
               <List.Item>
                 <List.Item.Meta
-                  avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                  title={<a href="https://ant.design">asdasd</a>}
+
+                  avatar={<TrophyOutlined style={{ color: 'yellow' }} />}
+                  title={<a href="https://ant.design">{item.title}</a>}
                   description="Ant Design, a design language for background applications, is refined by Ant UED Team"
                 />
               </List.Item>
